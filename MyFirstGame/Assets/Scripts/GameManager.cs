bd4a1c6d8a8a83c16aeb9a0480e3904e26a5cf67
itZSaint099 <soumyajithalder099@gmail.com>
2020-03-08 14:15:30 +0530

    Added next level and level complete trigger. Moved the endtrigger and level complete scripts to scripts folder.

--- MyFirstGame/Assets/Scripts/GameManager.cs
+++ MyFirstGame/Assets/Scripts/GameManager.cs
@@ -6,12 +6,19 @@ public class GameManager : MonoBehaviour
     bool endGame = false;
 
     public float restartDelay = 1f;
+
+    public GameObject completeLevelUI;
+    
+    public void CompleteLevel ()
+    {
+        completeLevelUI.SetActive(true);
+    }
     public void GameOver()
     {
         if(endGame == false)
         {
             endGame = true;
-            Debug.Log("GAME OVER");
+            //Debug.Log("GAME OVER");
             // Restart Ganme
             Invoke("Restart", restartDelay);
         }
